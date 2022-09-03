d4df17e3f4e6191478bc53adb7ac77a52d10e18d
sl4b <57822954+sl4b@users.noreply.github.com>
2022-09-03 00:17:55 -0400

    Better naming, error handling, add lotyr_route fn

--- lotyr.cpp
+++ lotyr.cpp
@@ -1,31 +1,70 @@
+#include <exception>
+#include <memory>
+
 #include <boost/property_tree/ptree.hpp>
 
 #include <valhalla/baldr/rapidjson_utils.h>
 #include <valhalla/tyr/actor.h>
+#include <valhalla/worker.h>
 
 #include "lotyr.h"
 
-thread_local valhalla::tyr::actor_t * actor;
+thread_local std::unique_ptr<valhalla::tyr::actor_t> actor;
+thread_local std::unique_ptr<std::string> response_str;
+thread_local std::unique_ptr<std::string> error_str;
 
-extern "C" void init(char * config_path) {
-  // Load the configuration
-  std::string config_file(config_path);
-  boost::property_tree::ptree config;
-  rapidjson::read_json(config_file, config);
+extern "C" int lotyr_init(char *config_path) {
+  try {
+    // Load the configuration
+    std::string config_file(config_path);
+    boost::property_tree::ptree config;
+    rapidjson::read_json(config_file, config);
 
-  // Delete the actor in case of repeated initialization
-  if (actor) {
-    delete actor;
-    actor = nullptr;
+    // Initialize the actor
+    actor = std::make_unique<valhalla::tyr::actor_t>(
+        valhalla::tyr::actor_t(config));
+
+    return 1;
+  } catch (valhalla::valhalla_exception_t e) {
+    error_str = std::make_unique<std::string>(e.message);
+    return 0;
+  } catch (...) {
+    error_str = nullptr;
+    return 0;
   }
-
-  // Initialize the actor
-  actor = new valhalla::tyr::actor_t(config);
 }
 
-extern "C" void deinit(void) {
-  if (actor) {
-    delete actor;
+extern "C" int lotyr_deinit(void) {
+  try {
     actor = nullptr;
+
+    return 1;
+  } catch (valhalla::valhalla_exception_t e) {
+    error_str = std::make_unique<std::string>(e.message);
+    return 0;
+  } catch (...) {
+    error_str = nullptr;
+    return 0;
   }
+}
+
+extern "C" const char *lotyr_route(char *request) {
+  try {
+    std::string request_str(request);
+    response_str = std::make_unique<std::string>(actor->route(request_str));
+    return response_str->c_str();
+  } catch (valhalla::valhalla_exception_t e) {
+    error_str = std::make_unique<std::string>(e.message);
+    return NULL;
+  } catch (...) {
+    error_str = nullptr;
+    return NULL;
+  }
+}
+
+extern "C" const char *lotyr_error(void) {
+  if (error_str)
+    return error_str->c_str();
+  else
+    return NULL;
 }
--- lotyr.h
+++ lotyr.h
@@ -7,13 +7,32 @@ extern "C" {
 
 /// Initializes the library, loading the Valhalla configuration from
 /// `config_path`.
-void init(char * config_path);
+///
+/// Returns 0 on failue, and a nonzero value otherwise.
+int lotyr_init(char *config_path);
 
 /// Deinitializes the library.
-void deinit(void);
+///
+/// Returns 0 on failure, and a nonzero value otherwise.
+int lotyr_deinit(void);
+
+/// Calculates a route described by `request`. DO NOT attempt to free the
+/// returned string. The returned string will be kept alive until `route` is
+/// called again, at which point the memory will be freed by the library.
+///
+/// Returns a `NULL` pointer on failure.
+///
+/// See the Valhalla API reference (linked below) for documentation on the
+/// format of `request`.
+/// https://valhalla.readthedocs.io/en/latest/api/turn-by-turn/api-reference/
+const char *lotyr_route(char *request);
+
+/// Gets the most recent error message, if any. The pointer returned by this function
+/// will only be valid until another error occurs, so copy it before use.
+const char *lotyr_error(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif  // LOTYR_LOTYR_H_
+#endif // LOTYR_LOTYR_H_
